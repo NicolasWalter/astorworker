d3c6d5ecbc1335ebccb32f319adca94ba1d9a4b6
Nicolás Walter <nicolas.walter@brubank.com>
2023-08-30 17:23:46 -0300

    add another R path

--- src/main/java/br/ufg/inf/astorworker/entities/AndroidProject.java
+++ src/main/java/br/ufg/inf/astorworker/entities/AndroidProject.java
@@ -216,7 +216,7 @@ public class AndroidProject {
 		}
 
 		dependencies += FileSystemUtils.fixPath(projectAbsolutePath + "/" + mainFolder + "/build/intermediates/compile_and_runtime_not_namespaced_r_class_jar/debug") + System.getProperty("path.separator");
-		//dependencies += FileSystemUtils.fixPath(projectAbsolutePath + "/" + mainFolder + "/build/generated/not_namespaced_r_class_sources/debug/processDebugResources") + System.getProperty("path.separator");
+		dependencies += FileSystemUtils.fixPath(projectAbsolutePath + "/" + mainFolder + "/build/generated/not_namespaced_r_class_sources/debug/processDebugResources") + System.getProperty("path.separator");
 		dependencies += FileSystemUtils.fixPath(projectAbsolutePath + "/" + mainFolder + "/build/intermediates/javac/debug/compileDebugJavaWithJavac/classes/") + System.getProperty("path.separator");
 
 		for(String entry : output){
